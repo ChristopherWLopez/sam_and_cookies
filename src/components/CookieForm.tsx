71266d8c92a261bffbd14737d51cbe36eb8499da
christopher lopez <christopherwlopez89@gmail.com>
2023-11-06 18:31:18 -0800

    added form for sam and cookies

--- src/components/CookieForm.tsx
+++ src/components/CookieForm.tsx
@@ -1,3 +1,29 @@
-import { useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { CookieStand } from "./CookieTable";
 
+import React from 'react'
+
+const CookieForm = () => {
+
+    const [location, setLocation] = useState('');
+  return (
+    <div>
+        <form onSubmit={(e: FormEvent<HTMLFormElement>)=>{
+            e.preventDefault();
+            console.log(e);
+        }}>
+            <label>
+                <input
+                    value={location}
+                    onChange={(e:ChangeEvent<HTMLInputElement>)=>{
+                        setLocation(e.target.value);
+                    }}/>
+            </label>
+            <button type="submit">submit</button>
+        </form>
+      
+    </div>
+  )
+}
+
+export default CookieForm
