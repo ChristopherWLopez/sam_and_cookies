2996a35b42b9daabceae60dcb0a4352a056665ba
christopher lopez <christopherwlopez89@gmail.com>
2023-11-06 18:51:13 -0800

    building form for cookit stand

--- src/components/CookieForm.tsx
+++ src/components/CookieForm.tsx
@@ -6,22 +6,59 @@ import React from 'react'
 const CookieForm = () => {
 
     const [location, setLocation] = useState('');
+    const [minCustomers, setMinCustomers]=useState(0);
+    const [maxCustomer, setMaxCustomers]=useState(0);
+    const [avgCookiePerSale, setavgCookiePerSale]=useState(0);
+
   return (
     <div>
         <form onSubmit={(e: FormEvent<HTMLFormElement>)=>{
             e.preventDefault();
+            const newStore = new CookieStand(location, 24, 34, 6.2);
             console.log(e);
         }}>
             <label>
                 <input
+                    type="number"
                     value={location}
                     onChange={(e:ChangeEvent<HTMLInputElement>)=>{
                         setLocation(e.target.value);
                     }}/>
             </label>
+            <label>
+                Min Customer{''}
+                <input
+                    value={minCustomers}
+                    onChange={(e:ChangeEvent<HTMLInputElement>)=>{
+                        setMinCustomers(Number(e.target.value));
+                    }}/>
+            </label>
+            <label>
+                Max Customer{''}
+                <input
+                    value={maxCustomer}
+                    onChange={(e:ChangeEvent<HTMLInputElement>)=>{
+                        setMaxCustomers(Number(e.target.value));
+                }}
+                />
+            </label>
+            <label>
+                Avg Cookie Per Customer{''}
+                <input
+                    value={avgCookiePerSale}
+                    onChange={(e:ChangeEvent<HTMLInputElement>)=>{
+                        setavgCookiePerSale(Number(e.target.value));
+                    }}
+                >
+                
+                </input>
+            </label>
             <button type="submit">submit</button>
         </form>
-      
+
+        <div>Location: {location}</div>
+        <div>Min Customers: {minCustomers}</div>
+        <div>Max Customers: {maxCustomer}</div>
     </div>
   )
 }
